cf6fd96c4058dec89388d95f5ebc3cd8c916a5eb
NoelDevs17 <noeldrum17@hotmail.com>
2024-05-20 19:53:09 -0400

    -Terminacion de tarea

---
+++ Practica_DI/CustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Practica_DI
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>();
+        }
+    }
+}
---
+++ Practica_DI/CustomerService.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Practica_DI
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public List<Customer> GetCustomers()
+        {
+            return _customerRepository.GetCustomers();
+        }
+
+    }
+}
